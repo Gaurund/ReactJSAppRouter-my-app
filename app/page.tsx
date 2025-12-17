4b2eab03604c8047c15bcbfc3b57a3461b76aa01
Gaurund <pavel.makushev@gmail.com>
2025-12-17 04:26:05 -0500

    Props 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,42 @@
-import Gallery from './Gallery';
+import { getImageUrl } from './utils.js';
 
-export default function App() {
+function Avatar({ person, size }) {
   return (
-    <Gallery />
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+export default function Profile() {
+  return (
+    <div>
+      <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi', 
+          imageId: 'YfeOqp2'
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: 'Aklilu Lemma', 
+          imageId: 'OKS67lh'
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{ 
+          name: 'Lin Lanying',
+          imageId: '1bX5QH6'
+        }}
+      />
+    </div>
   );
 }
